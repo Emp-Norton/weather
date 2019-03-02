2ed23c968137b1c3226b1b2d18e48da9dbed9737
James Jelenko <jamesjelenko@gmail.com>
2019-03-02 14:04:58 -0800

    Refactor handling of whitespace in urls

--- weather.js
+++ weather.js
@@ -1,13 +1,15 @@
 'use strict'
 const fetch = require('node-fetch');
 require('dotenv').config();
+
 const KEY = process.env.WEATHER_KEY;
 const CITY = process.argv.slice(2)
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${CITY},us&APPID=${KEY}`;
 
+const cityString = CITY.join('%20')
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityString},us&APPID=${KEY}`;
 
 const getWeather = () => {
-  url = encodeURI(url)
+  console.log(`fetching ${url}`)
   fetch(url)
     .then(function(response) {
       return response.json()

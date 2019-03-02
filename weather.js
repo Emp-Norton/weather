391cdb60178c7fd663ceba3f2a92f4256ad44158
James Jelenko <jamesjelenko@gmail.com>
2019-03-02 14:59:12 -0800

    Add semi-colons

--- weather.js
+++ weather.js
@@ -1,22 +1,24 @@
-'use strict'
+'use strict';
+const mailer = require('./mailerService.js');
+const logger = require('./historyService.js');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
 const KEY = process.env.WEATHER_KEY;
-const CITY = process.argv.slice(2)
+const CITY = process.argv.slice(2);
 
-const cityString = CITY.join('%20')
+const cityString = CITY.join('%20');
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityString},us&APPID=${KEY}`;
 
 const getWeather = () => {
-  console.log(`fetching ${url}`)
+  console.log(`fetching ${url}`);
   fetch(url)
     .then(function(response) {
       return response.json()
     })
     .then(function(data) {
-      console.log(data)
+      console.log(data);
     })
 }
 
-getWeather()
+getWeather();

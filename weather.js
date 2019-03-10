224831b63bb6a5127bca13257fd409a816e2be9c
Emp-Norton <jamesjelenko@gmail.com>
2019-03-09 18:23:16 -0800

    Disable logger until bug fix, add on-off module for LED weather calendar prototype, add error handling for getWeather promise

--- weather.js
+++ weather.js
@@ -2,6 +2,9 @@
 const mailer = require('./mailerService.js');
 const logger = require('./historyService.js');
 const fetch = require('node-fetch');
+const gpio = require('onoff').Gpio;
+const rainLed = new gpio(17, 'out')
+const sunLed = new gpio(22, 'out');
 require('dotenv').config();
 
 const KEY = process.env.WEATHER_KEY;
@@ -9,6 +12,14 @@ const CITY = process.argv.slice(2);
 
 const cityString = CITY.join('%20');
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityString},us&APPID=${KEY}`;
+
+const parseWeatherResponse = (response) => {
+  const weather = response.weather[0];
+  if (weather['main'] == 'Rain') {
+    return 'rain'
+  }
+  return 'sun'
+}
 
 const getWeather = () => {
   console.log(`fetching ${url}`);
@@ -18,6 +29,20 @@ const getWeather = () => {
     })
     .then(function(data) {
       console.log(data);
+      let weather = parseWeatherResponse(data);
+      console.log(weather);
+      if (weather == 'rain') {
+        rainLed.writeSync(1)
+      }
+      if (weather == 'sun') {
+        sunLed.writeSync(1)
+      }
+//      logger.writeLog('weather', data)
+      return data
+    })
+    .catch(function(error){
+      console.log('Gone fucky');
+      console.log(error);
     })
 }
 
